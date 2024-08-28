45349146a7402a23be3fd401bc2ec3842d8a5230
Marcos Silva de Souza <117598188+marcossouza4@users.noreply.github.com>
2024-08-28 19:26:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function contar() {
+    let inicio = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
+
+    // Verifica se algum dos campos está vazio
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!');
+    }else{
+        res.innerHTML = 'Contando:'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
+        } else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
